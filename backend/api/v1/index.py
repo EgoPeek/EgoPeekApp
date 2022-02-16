0dde5c2bfcb4aa9de8ed0667919026e8df9a4727
natebush707 <natebush707@gmail.com>
2022-02-16 09:06:48 -0800

    Add endpoint for basic user creation/insertion into EgoPeek db.

--- backend/api/v1/index.py
+++ backend/api/v1/index.py
@@ -1,12 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from backend import schemas
+from backend.database import get_database, models, engine
+from sqlalchemy.orm.session import Session
+from backend.database.db_user import create_user
 
 router = APIRouter()
 
+models.Base.metadata.create_all(engine)
 
-@router.get("")
+
+@router.get("/")
 def read_index():
     """
     Retrieve index
     """
     return {"Hello": "World"}
+
+
+@router.post('/users', response_model=schemas.UserDisplay)
+def create_new_user(request: schemas.UserBase, database: Session = Depends(get_database)):
+    """
+    Creates a new user in the EgoPeek database.
+    Inputs: string: username, string: email, string: password
+    Outputs: string: username, string: email
+    """
+    return create_user(database, request)
