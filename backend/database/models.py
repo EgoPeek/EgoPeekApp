3fc20f01956742d5db9a619ae59f0e463b7da38b
natebush707 <natebush707@gmail.com>
2022-02-28 22:39:27 -0800

    extend column for email to 60 characters

--- backend/database/models.py
+++ backend/database/models.py
@@ -5,5 +5,5 @@ class DbUser(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(15))
-    email = Column(String(25))
+    email = Column(String(60))
     password = Column(String(15))
