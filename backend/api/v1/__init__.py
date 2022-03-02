e77d554c7806c1a968c873250c3f7463ce8aad47
natebush707 <natebush707@gmail.com>
2022-03-01 19:15:51 -0800

    Modify base route for backend and user router.

--- backend/api/v1/__init__.py
+++ backend/api/v1/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from backend.api.v1 import index
+from backend.api.v1 import user
 
 api_router = APIRouter()
-api_router.include_router(index.router, prefix="/backend", tags=["backend"])
+api_router.include_router(user.router, prefix='/users',tags=["user"])
--- backend/core/config.py
+++ backend/core/config.py
@@ -1,7 +1,7 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    API_V1_STR: str = "/backend/api/v1"
+    API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str = "Ego Peek"
     PORT: int = 5000
 
