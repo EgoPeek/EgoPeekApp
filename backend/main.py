389b4235b7c863010327e1b5621a4fb18a87e6eb
natebush707 <natebush707@gmail.com>
2022-03-01 19:20:53 -0800

    Serve front end from Jinja template.

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,23 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from backend.api.v1 import api_router
 from backend.core import settings
 from fastapi.staticfiles import StaticFiles
 from FrontEnd import build
+from fastapi.responses import RedirectResponse, HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 backend = FastAPI(title=settings.PROJECT_NAME)
 backend.include_router(api_router, prefix=settings.API_V1_STR)
+templates = Jinja2Templates(directory="FrontEnd/build")
 
-# mount index.html for front end
-backend.mount("/app", StaticFiles(directory="FrontEnd/build", html = True), name="build")
+# mount index for front end
+backend.mount("/static", StaticFiles(directory="FrontEnd/build/static", html = True), name="static")
+
+# serve front end from root
+@backend.get("/", response_class = HTMLResponse)
+async def serve_frontend(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 if __name__ == "__main__":
     uvicorn.run(backend, host='0.0.0.0', port=settings.PORT)
