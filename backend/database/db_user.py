0284a4dea358b19ad1f6788364074b4514291354
natebush707 <natebush707@gmail.com>
2022-03-01 19:21:58 -0800

    First pass at database interface for user CRUD endpoints.

--- backend/database/db_user.py
+++ backend/database/db_user.py
@@ -2,8 +2,13 @@ from backend import schemas
 from sqlalchemy.orm.session import Session
 from .models import DbUser
 
+"""
+    TO DO:
+    1. Add exception handling
+    2. Add password hashing
+"""
 
-def create_user(db: Session, request: schemas.UserBase):
+def create_user(db: Session, request: schemas.UserRequest):
     print('creating new user')
     new_user = DbUser(
         username = request.username,
@@ -14,3 +19,29 @@ def create_user(db: Session, request: schemas.UserBase):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+def get_all_user_data(db:Session):
+    return db.query(DbUser).all()
+
+
+def get_user_data(db:Session, username: str):
+    return db.query(DbUser).filter(DbUser.username == username).first()
+
+
+def update_user_data(db:Session, username: str, request: schemas.UserRequest):
+    user = db.query(DbUser).filter(DbUser.username == username)
+    user.update({
+        DbUser.username: request.username,
+        DbUser.email: request.email,
+        DbUser.password: request.password # HASH THIS
+    })
+    db.commit()
+    return{'success': True}
+
+
+def delete_user_data(db: Session, username: str):
+    user = db.query(DbUser).filter(DbUser.username == username).first()
+    db.delete(user)
+    db.commit()
+    return{'success': True}
