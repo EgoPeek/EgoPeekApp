335469773af0f94958ed03c15d4245684240ee20
natebush707 <natebush707@gmail.com>
2022-03-06 17:52:53 -0800

    Add comment router to server.

--- backend/api/v1/__init__.py
+++ backend/api/v1/__init__.py
@@ -1,9 +1,15 @@
 from fastapi import APIRouter
 
-from backend.api.v1 import user, post
+from backend.api.v1 import tag, user, post, comment
 
 user_router = APIRouter()
-user_router.include_router(user.router, prefix='/users',tags=["user"])
+user_router.include_router(user.router, prefix='/users',tags=['user'])
 
 post_router = APIRouter()
-post_router.include_router(post.router, prefix='/posts',tags=["post"])
+post_router.include_router(post.router, prefix='/posts',tags=['post'])
+
+comment_router = APIRouter()
+comment_router.include_router(comment.router, prefix='/comments',tags=['comment'])
+
+tag_router = APIRouter()
+tag_router.include_router(tag.router, prefix='/tags',tags=['tags'])
--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,34 @@
 import uvicorn
 from fastapi import FastAPI, Request
-from backend.api.v1 import user_router, post_router
+from backend.api.v1 import user_router, post_router, comment_router, tag_router
 from backend.auth import auth_router
 from backend.core import settings
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from backend.database import models, engine
+from fastapi.middleware.cors import CORSMiddleware
+
+
+# initialize database engine
+models.Base.metadata.create_all(engine)
 
 # build routers
 backend = FastAPI(title=settings.PROJECT_NAME)
 backend.include_router(user_router, prefix=settings.API_V1_STR)
 backend.include_router(auth_router, prefix=settings.API_V1_STR)
 backend.include_router(post_router, prefix=settings.API_V1_STR)
+backend.include_router(comment_router, prefix=settings.API_V1_STR)
+backend.include_router(tag_router, prefix=settings.API_V1_STR)
+
+# handle CORS error for local development across ports
+backend.add_middleware(
+    CORSMiddleware,
+    allow_origins = ['http://localhost:3000'],
+    allow_credentials = True,
+    allow_methods = ['*'],
+    allow_headers = ['*']
+)
 
 # template for front end static file
 templates = Jinja2Templates(directory="FrontEnd/build")
