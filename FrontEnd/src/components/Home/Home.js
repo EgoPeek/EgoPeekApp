432be4b9cdcbc99ffa6e9a942c25c16450ee04b3
Axel-LiraR <99221025+Axel-LiraR@users.noreply.github.com>
2022-03-08 19:13:36 -0800

    Fixed home page styling

--- FrontEnd/src/components/Home/Home.js
+++ FrontEnd/src/components/Home/Home.js
@@ -1,39 +1,51 @@
 import React from "react";
-/*import { useEffect } from "react"; */
 import { GreenButton } from "../Misc/Buttons";
 import "./Home.css";
 import homeIMG from "../../images/Home.jpg";
+import { FormControl } from "@mui/material";
 
 const Home = () => {
   return (
     <div className="home">
-      <h1> EgoPeek </h1>
-      <div className="homeIMG-container">
-        <img src={homeIMG} width="700rem" height="900rem"></img>
+      <div className="home-header">
+        <h1>EgoPeek</h1>
       </div>
 
-      <GreenButton className="login-button" variant="outlined">
-        Login
-      </GreenButton>
-      <GreenButton className="signup-button" variant="outlined">
-        Sign Up
-      </GreenButton>
+      <div className="home-left">
+        <div className="home-info">
+          <h2>Discover content from the games you love!</h2>
+          <h3>Log in or Sign up below!</h3>
+        </div>
 
-      <div className="home-description">
-        <span>
-          {" "}
-          Ego Peek is a Social Media app designed and Engineered by Kevin Gomez,
-          Giovanni Toxqui, Nathan Bush, Samuel Martinez, Steve Flores, and Axel
-          Lira. Our goal is to connect gamers from all around allowing anyone to
-          share their highlights and keep up with the latest gaming news!{" "}
-        </span>
+        <div className="home-button">
+          <div className="button-spacing">
+            <GreenButton className="login-button" variant="outlined">
+              Login
+            </GreenButton>
+            <GreenButton className="signup-button" variant="outlined">
+              Sign up
+            </GreenButton>
+          </div>
+        </div>
       </div>
 
-      <div className="home-intro">
-        <h2> Discover content from the games you love! </h2>
+      <div className="home-right">
+        <div className="home-Img">
+          <img src={homeIMG} width="100%" height="950rem"></img>
+        </div>
       </div>
-      <div className="home-intro2">
-        <h3> Log in or Sign up below! </h3>
+
+      <div className="home-footer">
+        <div className="footer-description">
+          <span>
+            {" "}
+            EgoPeek is a Social Media app designed and engineered by Kevin
+            Gomez, Giovanni Toxqui, Nathan Bush, Samuel Martinez, Steve Flores,
+            and Axel Lira. Our goal is to connect gamers from all around
+            allowing anyone to share their highlights and keep up with the
+            latest gaming news.{" "}
+          </span>
+        </div>
       </div>
     </div>
   );
