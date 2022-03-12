4bdcbfd7e5c1a43ae135eb277820c66db07a2a73
Axel-LiraR <99221025+Axel-LiraR@users.noreply.github.com>
2022-03-11 19:51:49 -0800

    Added basic structure

--- FrontEnd/src/components/Account/Account.js
+++ FrontEnd/src/components/Account/Account.js
@@ -1,1 +1,44 @@
+import React from "react";
+import "./Account.css";
+import Header from "../Misc/CustomComponents/Header";
 
+const Account = () => {
+  return (
+    <div className="account">
+      {/*<div className="header">
+        <Header />
+  </div>*/}
+
+      <div className="account-container">
+        <div className="account-hamburger">
+          <div className="account-info">
+            <span>account pic, contact, etc</span>
+          </div>
+
+          <div className="account-bio">
+            <h1>bio goes here</h1>
+          </div>
+
+          <div className="account-favorites">
+            <h1>Favorite Games</h1>
+            <div className="favorite-spacing">
+              {/* import favorite games */}
+              <span>
+                <p>game1</p>
+                <p>game2</p>
+                <p>game3</p>
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="posts-container">
+          <div className="posts">
+            <span>posts go here</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Account;
