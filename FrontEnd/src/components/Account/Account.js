cfbe996e12647ae1d829723d545f0e87e5ec35f6
Axel-LiraR <99221025+Axel-LiraR@users.noreply.github.com>
2022-03-14 21:03:10 -0700

    Updated Account page

--- FrontEnd/src/components/Account/Account.js
+++ FrontEnd/src/components/Account/Account.js
@@ -1,22 +1,22 @@
 import "./Account.css";
 import "./Sidebar.js";
-import React, { Component } from "react";
+import React from "react";
 import Sidebar from "./Sidebar";
-import Posts from "./Posts";
+import Posts from "../Misc/CustomComponents/UserPost";
 import Header from "../Misc/CustomComponents/Header";
 
-class Account extends Component {
-  render() {
-    return (
-      <div className="account-page">
-        {/*<Header />*/}
-        <div className="account-main">
-          <Sidebar />
+const Account = () => {
+  return (
+    <div className="account-page">
+      <Header />
+      <div className="account-main">
+        <Sidebar />
+        <div className="posts-container">
           <Posts />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Account;
