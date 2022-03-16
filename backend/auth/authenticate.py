5bcb178d6970e885592efecac424b4c91cdc35c4
natebush707 <natebush707@gmail.com>
2022-03-15 17:10:47 -0700

    Add 401 unauthorized for invalid login.

--- backend/auth/authenticate.py
+++ backend/auth/authenticate.py
@@ -4,12 +4,13 @@ authenticate.py
     - future: oauth2 authentication implementation
 """
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from backend.database import get_database
 from sqlalchemy.orm.session import Session
 from backend.database.models import DbUser
 from backend.database.hash import Hash
 from backend import schemas
+from fastapi.exceptions import HTTPException
 
 router = APIRouter()
 
@@ -23,8 +24,9 @@ def login(request: schemas.LoginRequest, database: Session = Depends(get_databas
     """
     user = database.query(DbUser).filter(DbUser.username == request.username).first()
     if not user:
-        return {'success': False, 'reason': 'Invalid username.'}
+        raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED,
+                            detail = 'Invalid username.')
     if not Hash.verify(user.password, request.password):
-        return {'success': False, 'reason': 'Invalid password.'}
-
+        raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED,
+                            detail = 'Invalid password.')
     return {'user_id': user.id, 'username': user.username}
