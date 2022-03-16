f7604a229fe2fed7b5262168fc44967ecf45990b
Axel-LiraR <99221025+Axel-LiraR@users.noreply.github.com>
2022-03-15 21:14:18 -0700

    added posts from db

--- FrontEnd/src/components/Account/Account.js
+++ FrontEnd/src/components/Account/Account.js
@@ -3,9 +3,13 @@ import "./Sidebar.js";
 import React from "react";
 import Sidebar from "./Sidebar";
 import UserPost from "../Misc/CustomComponents/UserPost";
+import "../Misc/CustomComponents/UserPost";
+import useFetch from "../../hooks/useFetch";
 import Header from "../Misc/CustomComponents/Header";
 
 const Account = () => {
+  const userID = window.localStorage.getItem("userID");
+  const { data: post } = useFetch(`/api/v1/posts/all/${userID}`);
   return (
     <div className="account-page">
       <div className="header-container">
@@ -14,7 +18,7 @@ const Account = () => {
       <div className="account-main">
         <Sidebar />
         <div className="posts-container">
-          <p>posts go here</p>
+          {post && post.map((item, i) => <UserPost post={item} key={i} />)}
         </div>
       </div>
     </div>
