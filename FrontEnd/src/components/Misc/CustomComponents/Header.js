ffd523497346e50427132e53cc2c4bc7635dfa34
Steve Flores <stevewflores43@gmail.com>
2022-03-31 21:01:07 -0700

    missed one lol

--- FrontEnd/src/components/Misc/CustomComponents/Header.js
+++ FrontEnd/src/components/Misc/CustomComponents/Header.js
@@ -5,25 +5,26 @@
 
 import React from 'react'
 import './Header.css'
-import {Link} from 'react-router-dom'
-import { IconBubble,MenuItem } from './IconBubble'
+import { Link } from 'react-router-dom'
+import { IconBubble, MenuItem } from './IconBubble'
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import SettingsIcon from '@mui/icons-material/Settings';
 import InboxIcon from '@mui/icons-material/Inbox';
 import LogoutIcon from '@mui/icons-material/Logout';
 import useAuth from '../../../hooks/useAuth';
 import useFetch from '../../../hooks/useFetch';
+import { CircularProgress } from '@mui/material';
 
 const Header = () => {
     const userName = window.localStorage.getItem('userName')
     const userID = window.localStorage.getItem('userID')
 
-    const {data,isPending,error} = useFetch(`/api/v1/profiles/avatar/${userID}`)
-    const {logout} = useAuth();
+    const { data, isPending, error } = useFetch(`/api/v1/profiles/avatar/${userID}`)
+    const { logout } = useAuth();
 
     return (
         <div className='header'>
-        
+
             <div className='header-information'>
                 <div className='header-title'>
                     <h1>EgoPeek</h1>
@@ -34,14 +35,19 @@ const Header = () => {
                 <Link to='/#' className='header-item'><p> Contact </p></Link>
             </div>
             {/* custom user profile thing will go here */}
-            <IconBubble imgStyle={{height:'6rem',width:'6rem'}} userImgSrc={!isPending ? data.avatar_path : '/'}>
-                <MenuItem MenuIcon={<AccountBoxIcon/>} redirect={`/${userName}`}>Account</MenuItem>
-                <MenuItem MenuIcon={<SettingsIcon/>} redirect='/settings'>Settings</MenuItem>
-                <MenuItem MenuIcon={<InboxIcon/>} redirect='#'>Messages</MenuItem>
-                <MenuItem MenuIcon={<LogoutIcon/>} onMouseDown={()=>{
-                    logout();
-                }} redirect="#">Log Out</MenuItem>
-            </IconBubble>
+            {
+                isPending || error
+                    ? <CircularProgress />
+                    :
+                    <IconBubble imgStyle={{ height: '6rem', width: '6rem' }} userImgSrc={data.avatar_path}>
+                        <MenuItem MenuIcon={<AccountBoxIcon />} redirect={`/${userName}`}>Account</MenuItem>
+                        <MenuItem MenuIcon={<SettingsIcon />} redirect='/settings'>Settings</MenuItem>
+                        <MenuItem MenuIcon={<InboxIcon />} redirect='#'>Messages</MenuItem>
+                        <MenuItem MenuIcon={<LogoutIcon />} onMouseDown={() => {
+                            logout();
+                        }} redirect="#">Log Out</MenuItem>
+                    </IconBubble>
+            }
         </div>
     )
 }
