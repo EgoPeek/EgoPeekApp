d91f2cc889c0ff5e27e0a37a198363e8a6c15d4b
Steve Flores <stevewflores43@gmail.com>
2022-03-31 18:47:47 -0700

    register page now adds custom tags when searched for

--- FrontEnd/src/components/Register/Interests.js
+++ FrontEnd/src/components/Register/Interests.js
@@ -2,7 +2,7 @@
  *  File name: INterests.js
  *  Description: Interest page where users can select what types of topics they're interested in viewing on their userFeed
  */
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { TextInputStandard } from '../Misc/Input/TextFields'
 import { GreenButton } from '../Misc/Input/Buttons'
 import { useNavigate } from 'react-router';
@@ -19,11 +19,12 @@ const InterestPage = () => {
     const userID = window.localStorage.getItem('userID')
     const navigate = useNavigate();
     const [tagData, setTagData] = useState([])
+    const [customTag, setCustomTag] = useState([])
 
     useEffect(() => {
-      setTagData(!tagPending ? tags : [])
+        setTagData(!tagPending ? tags : [])
     }, [tagPending])
-    
+
 
     const updateTag = (tagName) => {
         //if the tag exists in the array remove it and put it back in the original
@@ -35,21 +36,38 @@ const InterestPage = () => {
 
         //if its brand new just add it and remove it from original array
         setSelectedTags([...selectedTags, tagName])
-        setTagData(tagData.filter(x=>x!==tagName))
+        setTagData(tagData.filter(x => x !== tagName))
     }
 
-    const Tag = ({ title }) => {
+    //for tags that the user searches so they don't get placed in the original array
+    const updateCustomTag = (tagName) => {
+        //if the tag exists in the array remove it
+        if (customTag.find(x => x === tagName)) {
+            setCustomTag(selectedTags.filter(x => x !== tagName))
+            return
+        }
+
+        //if its brand new just add it and remove it from original array
+        setCustomTag([...selectedTags, tagName])
+    }
+
+    const Tag = ({ title, updateFunction }) => {
         return (
-            <div className='tag' onClick={() => updateTag(title)}>
+            <div className='tag' onClick={() => updateFunction(title)}>
                 <p>{title}</p>
             </div>
         )
     }
 
     const handleSearchTag = (e) => {
-        if (e.key !== 'enter') return
+        if (e.key !== 'Enter' || searchTag === '') return
+        const tag = formatString(searchTag)
 
-        console.log('search tag')
+        let test = []
+        if (!selectedTags.find(x => x === tag)) setCustomTag([...customTag, tag])
+        setSearchTag('')
+
+        console.log('search tag:', searchTag)
     }
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -62,26 +80,35 @@ const InterestPage = () => {
             })
             navigate('/')
 
-        }catch(err){
+        } catch (err) {
             console.log(err)
         }
     }
+    const formatString = (val) => {
+        //yes this is a thing, all it does it split the string and create each word uppercased
+        // ex. dying of the light => Dying Of The Light, just to make the purple pills nicer
+        const concatStr = val.split(' ')
+            .map(str => str[0].toUpperCase() + str.slice(1))
+            .join(' ')
+        return concatStr
+    }
 
     return (
-        <form className='form-container' onSubmit={handleSubmit}>
+        <div className='form-container'>
             <h2>Interests</h2>
             <p>Pick games that interest you the most to receieve a more personalized feed</p>
-            <TextInputStandard size='small' sx={{ width: '70%' }} label="Search by tag" onChange={e => setSearchTag(e.target.value)} onKeyPress={handleSearchTag} />
+            <TextInputStandard autoComplete='off' value={searchTag} size='small' sx={{ width: '70%' }} label="Search by tag" onChange={(e) => setSearchTag(e.target.value)} onKeyPress={handleSearchTag} />
             <div className='tag-container'>
                 <div className='chosen-tags interests'>
-                    {selectedTags.map((x, i) => <Tag title={x} key={i} />)}
+                    {selectedTags.map((x, i) => <Tag title={x} updateFunction={updateTag} key={i} />)}
+                    {customTag.map((x, i) => <Tag title={x} updateFunction={updateCustomTag} key={i} />)}
                 </div>
                 <div className='interests'>
-                    {!tagPending && tagData.map((item, i) => <Tag title={item} key={i} />)}
+                    {!tagPending && tagData.map((item, i) => <Tag title={item} updateFunction={updateTag} key={i} />)}
                 </div>
             </div>
-            <GreenButton variant='outlined' type='submit'>Submit</GreenButton>
-        </form>
+            <GreenButton variant='outlined' onClick={handleSubmit}>Submit</GreenButton>
+        </div>
     )
 }
 
