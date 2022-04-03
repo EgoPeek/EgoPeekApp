cb4d4ec4099af2ad47420f13c3586e5d992658d5
Samuel Martinez <sammartinez100299@gmail.com>
2022-04-02 18:54:09 -0700

    add reset password email endpoint

--- backend/auth/authenticate.py
+++ backend/auth/authenticate.py
@@ -4,16 +4,18 @@ authenticate.py
     - future: oauth2 authentication implementation
 """
 
-from email.policy import HTTP
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, BackgroundTasks
 from backend.auth.oauth import create_access_token
-from backend.database import get_database
+from backend.database import get_database, db_user
 from sqlalchemy.orm.session import Session
 from backend.database.models import DbUser
 from backend.database.hash import Hash
 from backend import schemas
 from fastapi.exceptions import HTTPException
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+#from send_email import send_email_background
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+from backend.core.config import email_config
 
 router = APIRouter()
 
@@ -33,6 +35,35 @@ def login(request: schemas.LoginRequest, database: Session = Depends(get_databas
         raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED,
                             detail = 'Invalid password.')
     return {'user_id': user.id, 'username': user.username}
+
+@router.post('/emails/reset-password/{user_id}')
+async def send_in_background(user_id, background_tasks: BackgroundTasks, database: Session = Depends(get_database)):
+    email = db_user.get_email_by_userID(database, user_id)
+    bosdy = {'name': 'sam', 'title': 'shreck'}
+    message = MessageSchema(
+        subject = "Password Reset Request",
+        recipients = [email],
+        template_body = bosdy,)
+    fm = FastMail(email_config)
+    background_tasks.add_task(fm.send_message, message, template_name = 'emails.html')
+
+    return {"success": True, "message": "email has been sent"}
+
+
+
+def send_email_background(background_tasks: BackgroundTasks, subject: str, email_to: str, body: str):
+    message = MessageSchema(
+        subject = subject,
+        recipients = [email_to],
+        body = body,
+        subtype = 'html',
+    )
+    fm = FastMail(email_config)
+    background_tasks.add_task(
+       fm.send_message, message, template_name = 'emails.html')
+
+    
+
 
 
 # @router.post('/login')
