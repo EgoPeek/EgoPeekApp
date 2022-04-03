1f1dd27e8f2f1952844cd6e9a13aa8758eab2e17
Samuel Martinez <sammartinez100299@gmail.com>
2022-04-02 18:55:06 -0700

    add configuration for gmail server

--- backend/core/config.py
+++ backend/core/config.py
@@ -4,6 +4,9 @@ config.py
 """
 
 from pydantic import BaseSettings
+from fastapi import BackgroundTasks
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+from backend import env 
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
@@ -11,3 +14,16 @@ class Settings(BaseSettings):
     PORT: int = 5000
 
 settings = Settings()
+
+email_config = ConnectionConfig(
+    MAIL_USERNAME=env.MAIL_USERNAME,
+    MAIL_PASSWORD=env.MAIL_PASSWORD,
+    MAIL_FROM=env.MAIL_FROM,
+    MAIL_PORT=env.MAIL_PORT,
+    MAIL_SERVER=env.MAIL_SERVER,
+    MAIL_FROM_NAME=env.MAIL_FROM_NAME,
+    MAIL_TLS=True,
+    MAIL_SSL=False,
+    USE_CREDENTIALS=True,
+    TEMPLATE_FOLDER='./backend/auth/templates'
+)
