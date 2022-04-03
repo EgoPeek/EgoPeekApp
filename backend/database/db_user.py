b38698a596302e86a60e43f9926544546df97e59
natebush707 <natebush707@gmail.com>
2022-04-03 16:32:10 -0700

    Add database functions for dealing with password reset tokens and updating passwords.

--- backend/database/db_user.py
+++ backend/database/db_user.py
@@ -4,6 +4,7 @@ db_user.py
     - functions from here are called by user-related CRUD endpoints
 """
 
+from pymysql import NULL
 from backend import schemas
 from sqlalchemy.orm.session import Session
 from .models import DbUser, DbProfile
@@ -16,7 +17,8 @@ def create_user(db: Session, request: schemas.UserRequest):
     new_user = DbUser(
         username = request.username,
         email = request.email,
-        password = Hash.encrypt(request.password)
+        password = Hash.encrypt(request.password),
+        reset = NULL
      )
     db.add(new_user)
     db.commit()
@@ -30,8 +32,10 @@ def create_user(db: Session, request: schemas.UserRequest):
     
     return new_user
 
+
 def get_email_by_userID(db:Session, user_id: str):
     return db.query(DbUser.email).filter(DbUser.id == user_id).first()[0]
+
 
 def get_all_user_data(db: Session):
     return db.query(DbUser).all()
@@ -64,6 +68,24 @@ def update_user_data(db: Session, username: str, request: schemas.UserRequest):
     return{'success': True, 'message': f'Updated user: {username}'}
 
 
+def update_user_reset_token(db: Session, username: str, token: str):
+    user = db.query(DbUser).filter(DbUser.username == username)
+    user.update({
+        DbUser.reset: token
+    })
+    db.commit()
+    return {'success': True, 'message': f'Updated reset token for {username}'}
+
+
+def update_user_password(db: Session, username: str, new_password: str):
+    user = db.query(DbUser).filter(DbUser.username == username)
+    user.update({
+        DbUser.password: Hash.encrypt(new_password)
+    })
+    db.commit()
+    return{'success': True, 'message': f'Updated password for user: {username}'}
+
+
 def delete_user_data(db: Session, username: str):
     user = db.query(DbUser).filter(DbUser.username == username).first()
     db.delete(user)
@@ -75,3 +97,7 @@ def check_duplicates(db: Session, username: str, email: str):
     if db.query(DbUser).filter(DbUser.username == username).first() or db.query(DbUser).filter(DbUser.email == email).first():
         return True
     return False
+
+
+def check_reset_token(db: Session, username: str, reset_string: str):
+    return db.query(DbUser.reset).filter(DbUser.username == username).first()[0] == reset_string
