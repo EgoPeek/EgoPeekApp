914f24fceec0695513d58fd02448827128157f6f
Steve Flores <stevewflores43@gmail.com>
2022-04-04 02:04:57 -0700

    pain and suffering

--- FrontEnd/src/components/UserFeed/UserFeed.js
+++ FrontEnd/src/components/UserFeed/UserFeed.js
@@ -3,14 +3,12 @@
  *  Description: Home screen for a logged in user, displays users friends, and receieve posts from other users
  */
 
-import { useState } from 'react'
 import Header from '../Misc/CustomComponents/Header'
 import '../Misc/CustomComponents/UserPost'
 import './UserFeed.css'
 import UserPost from '../Misc/CustomComponents/UserPost'
 import useFetch from '../../hooks/useFetch'
 import CreatePost from './CreatePost'
-import DisplayPost from './DisplayPost'
 import { GreenLoadingBar } from '../Misc/Input/LoadingBar';
 import FriendsList from './FriendsList'
 
@@ -22,7 +20,7 @@ const UserFeed = () => {
     const PostList = () => {
         if (postError) return <></>
 
-        if (post) return post.map((item, i) => <UserPost onClick={() => displayPost(item)} post={item} key={i} />)
+        if (post) return post.map((item, i) => <UserPost post={item} key={i} />)
         else return <></>
     }
 
