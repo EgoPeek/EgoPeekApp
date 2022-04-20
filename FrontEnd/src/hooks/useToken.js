a573f26e1d8f9ba686fdd45c8d0e9cb3184af5ce
natebush707 <natebush707@gmail.com>
2022-04-20 15:49:49 -0700

    Fix useToken hook for retrieving current validated userID

--- FrontEnd/src/hooks/useToken.js
+++ FrontEnd/src/hooks/useToken.js
@@ -5,10 +5,8 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-const authHeader = window.localStorage.getItem('token_type') + " " + window.localStorage.getItem('token')
-
 // custom hook that will fetch the userID associated with the current token
-const useToken = async (go) => {
+const useToken = () => {
     const [data, setData] = useState(null)
     const [isPending, setIsPending] = useState(true)
     const [error, setError] = useState(null)
@@ -16,19 +14,17 @@ const useToken = async (go) => {
     useEffect(() => {
         const validateUser = async () => { 
             try {
-                const response = await axios.get(`/api/v1/users/me/`, {headers: {Authorization: authHeader}})
+                const response = await axios.get(`/api/v1/users/me/`, {headers: {Authorization: window.localStorage.getItem('token_type') + " " + window.localStorage.getItem('token')}})
                 setData(response.data.id)
                 setIsPending(false)
-                // return response.data.id
             } catch (error) {
                 setError(error.message)
                 setIsPending(false)
             }
         }
         validateUser();
-    }, [go])
+    }, [])
 
-    console.log(data, isPending, error, 'DATA FROM HOOK')
     return { data, isPending, error }
 }
 
