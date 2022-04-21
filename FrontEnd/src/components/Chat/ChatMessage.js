71fa76474817a0cc50213e5b06457329878513d3
Steve Flores <stevewflores43@gmail.com>
2022-04-21 00:46:26 -0700

    fixed some formatting on chat message

--- FrontEnd/src/components/Chat/ChatMessage.js
+++ FrontEnd/src/components/Chat/ChatMessage.js
@@ -5,10 +5,14 @@ import './Chat.css'
 
 
 const ChatMessage = ({ body, username, avatar, isSelf, time, msgKey }) => {
+    const dateObj = new Date(time)
+    const date = `${dateObj.getFullYear()}/${dateObj.getMonth() + 1}/${dateObj.getDate()}`
+
+
     if (isSelf) {
         return (<div className='my-chat-bubble'>
             <div className='my-chat-message' style={{ textAlign: 'right' }} key={msgKey}>
-                {time}    {username}<br />{body}
+                {date} {username}<br />{body}
             </div>
             <IconBubble className='bubble' imgStyle={{ height: '3rem', width: '3rem' }} userImgSrc={avatar}></IconBubble>
         </div>
@@ -19,7 +23,7 @@ const ChatMessage = ({ body, username, avatar, isSelf, time, msgKey }) => {
         <div className='other-chat-bubble'>
             <IconBubble className='bubble' imgStyle={{ height: '3rem', width: '3rem' }} userImgSrc={avatar}></IconBubble>
             <div className={'other-chat-message'} style={{ textAlign: 'left' }} key={msgKey}>
-                {time}    {username}<br />{body}
+                {date} {username}<br />{body}
             </div>
         </div>
     )
