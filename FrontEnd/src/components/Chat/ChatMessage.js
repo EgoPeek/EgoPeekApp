8ec071878f1a06f2f3f4afc162eca3802852373f
Steve Flores <stevewflores43@gmail.com>
2022-04-25 18:50:17 -0700

    fixed chat messages not displaying time

--- FrontEnd/src/components/Chat/ChatMessage.js
+++ FrontEnd/src/components/Chat/ChatMessage.js
@@ -5,14 +5,11 @@ import './Chat.css'
 
 
 const ChatMessage = ({ body, username, avatar, isSelf, time, msgKey }) => {
-    const dateObj = new Date(time)
-    const date = `${dateObj.getFullYear()}/${dateObj.getMonth() + 1}/${dateObj.getDate()}`
-
 
     if (isSelf) {
         return (<div className='my-chat-bubble'>
             <div className='my-chat-message' style={{ textAlign: 'right' }} key={msgKey}>
-                {date} {username}<br />{body}
+                {time} {username}<br />{body}
             </div>
             <IconBubble className='bubble' imgStyle={{ height: '3rem', width: '3rem' }} userImgSrc={avatar}></IconBubble>
         </div>
@@ -23,7 +20,7 @@ const ChatMessage = ({ body, username, avatar, isSelf, time, msgKey }) => {
         <div className='other-chat-bubble'>
             <IconBubble className='bubble' imgStyle={{ height: '3rem', width: '3rem' }} userImgSrc={avatar}></IconBubble>
             <div className={'other-chat-message'} style={{ textAlign: 'left' }} key={msgKey}>
-                {date} {username}<br />{body}
+                {time} {username}<br />{body}
             </div>
         </div>
     )
