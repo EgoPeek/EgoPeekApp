a85cc8b01b59e99262c8206fd8800b3717140df2
Steve Flores <stevewflores43@gmail.com>
2022-05-02 18:25:08 -0700

    added about us page

---
+++ FrontEnd/src/components/About/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../Misc/CustomComponents/Header'
+import './About.css'
+
+const About = () => {
+    return (
+        <>
+            <Header />
+            <div className='about-us'>
+                About
+            </div>
+        </>
+    )
+}
+
+export default About
