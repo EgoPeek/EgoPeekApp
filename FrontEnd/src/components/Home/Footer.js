e74bbed2345163498873f099c74ace89d460ac1e
Steve Flores <stevewflores43@gmail.com>
2022-05-10 23:28:23 -0700

    added a sick ass footer

---
+++ FrontEnd/src/components/Home/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <footer className='home-footer'>
+      <div className='about-container'>
+        <div className='footer-row '>
+          <div className='footer-about footer-box'>
+            <h1>About</h1>
+            <p className="text-justify">
+              EgoPeek is a mock social media site created by a group of students who currently attend CSUSB.
+              Our goal was to connect people with a passion for gaming. Similar to other Social Media Platforms
+              EgoPeek allows users to create posts, connect with friends, and personalize their own feed. Our 
+              <s><i><strong style={{fontSize:'1.5rem'}}>"very"</strong></i></s>
+              sophisticatedalgorithm knows exactly what type of content to provide every user based on their chosen interests.
+            </p>
+          </div>
+          <div className='footer-link footer-box'>
+            <h1>Links</h1>
+            <a href='https://github.com/EgoPeek/EgoPeekApp'>Github</a>
+            <a href='/about'>Developers</a>
+            <a>Twitter</a>
+            <a>Instagram</a>
+            <a>Facebook</a>
+          </div>
+        </div>
+        <hr />
+      </div>
+
+      <div className='about-container'>
+        <div className='footer-info footer-box'>
+          <p class="copyright-text">Copyright &copy; 2022 All Rights Reserved by EgoPeek</p>
+          <p>All games depicted within EgoPeek are in no way affiliated</p>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- FrontEnd/src/components/Home/Home.js
+++ FrontEnd/src/components/Home/Home.js
@@ -12,6 +12,7 @@ import "./Home.css";
 import Slideshow from "./Slideshow";
 import { Photo } from "./Slideshow";
 import EGOPEEKLOGO from '../../images/EGOPEEK_CLEAR.png'
+import Footer from "./Footer";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -34,55 +35,46 @@ const Home = () => {
   };
 
   return (
-    <div className="home">
-      <div className="home-container">
-        <div className="home-left">
-          <div className="home-header">
-            <img src={EGOPEEKLOGO}/>
-            <h1>EgoPeek</h1>
-          </div>
-          <div className="home-info">
-            <h2>Discover content from the games you love!</h2>
-            <h3>Log in or Sign up below!</h3>
+    <>
 
-            <div className="home-button-spacing">
-              <GreenButton
-                className="login-button"
-                variant="outlined"
-                onClick={redirectToLogin}
-              >
-                Login
-              </GreenButton>
-              <GreenButton
-                className="signup-button"
-                variant="outlined"
-                onClick={redirectToRegister}
-              >
-                Sign up
-              </GreenButton>
+      <div className="home">
+        <div className="home-container">
+          <div className="home-left">
+            <div className="home-header">
+              <img src={EGOPEEKLOGO} />
+              <h1>EgoPeek</h1>
+            </div>
+            <div className="home-info">
+              <h2>Discover content from the games you love!</h2>
+              <h3>Log in or Sign up below!</h3>
+
+              <div className="home-button-spacing">
+                <GreenButton
+                  className="login-button"
+                  variant="outlined"
+                  onClick={redirectToLogin}
+                >
+                  Login
+                </GreenButton>
+                <GreenButton
+                  className="signup-button"
+                  variant="outlined"
+                  onClick={redirectToRegister}
+                >
+                  Sign up
+                </GreenButton>
+              </div>
+            </div>
+          </div>
+          <div className="home-right">
+            <div className="home-img-spacing">
+              <Slideshow />
             </div>
           </div>
         </div>
-        <div className="home-right">
-          <div className="home-img-spacing">
-            <Slideshow />
-          </div>
-        </div>
-      </div>
-
-      <div className="home-footer">
-        <div className="footer-description">
-          <span>
-            {" "}
-            EgoPeek is a Social Media app designed and engineered by Kevin
-            Gomez, Giovanni Toxqui, Nathan Bush, Samuel Martinez, Steve Flores,
-            and Axel Lira. Our goal is to connect gamers from all around
-            allowing anyone to share their highlights and keep up with the
-            latest gaming news.{" "}
-          </span>
-        </div>
-      </div>
-    </div>
+      </div >
+      <Footer />
+    </>
   );
 };
 export default Home;
